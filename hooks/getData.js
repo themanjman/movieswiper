3aff00243c0e0729c447aee72cc9ef8f84d21364
themanjman <manj_man@hotmail.co.uk>
2022-07-02 14:56:24 +0100

    added custom hook to get data using tmdb API

---
+++ hooks/getData.js
@@ -0,0 +1,26 @@
+import {React,useState,useEffect} from 'react'
+import axios from 'axios';
+
+function getMovies() {
+  const [apiData, setApiData] = useState(null);
+  const [serverError, setServerError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const resp = await axios.get(process.env.NEXT_PUBLIC_MOVIE_URL);
+        const data = await resp?.data.results;
+        console.log(data);
+        setApiData(data);
+      } catch (error) {
+        setServerError(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return { apiData, serverError };
+}
+
+export default getMovies
